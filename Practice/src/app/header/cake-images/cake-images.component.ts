b2a5302d4c7617eaa6cd04e08b050e6b3f5c0871
srajput2 <sourabh.rajput9232@gmail.com>
2022-05-25 11:46:37 -0700

    Images and Header styling

--- Practice/src/app/header/cake-images/cake-images.component.ts
+++ Practice/src/app/header/cake-images/cake-images.component.ts
@@ -8,7 +8,12 @@ import { cakeDetails } from 'src/app/cakeDetails.model';
 })
 export class CakeImagesComponent implements OnInit {
   cakeImage: cakeDetails[] =[
-    new cakeDetails('/cake/030.jpg')
+    new cakeDetails('../../assets/030.jpg'),
+    new cakeDetails('../../assets/71x0no3astc21.webp'),
+    new cakeDetails('../../assets/900_755567gfsP_christmas-wedding-cake.jpg'),
+    new cakeDetails('../../assets/1200px-Wedding_cake_with_pillar_supports,_2009.jpg'),
+    new cakeDetails('../../assets/Glamorous-Wedding-Cake.jpg'),
+    new cakeDetails('../../assets/Heartsweet.webp')
   ];
   constructor() { }
 
